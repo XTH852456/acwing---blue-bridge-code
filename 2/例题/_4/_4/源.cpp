ec5cf4fe817b083f47e5ad6b11826a559ae04cf3
XUTAINHE <932898583@qq.com>
2023-09-05 21:55:33 +0800

    徐天禾

--- 2/例题/_4/_4/源.cpp
+++ 2/例题/_4/_4/源.cpp
@@ -11,13 +11,19 @@ int S[1001][1001];
 int main (void){
 	cin >> n >> m >> q;
 
-	for (int i = 0;i < n;i++) {
-		for (int j=0;j < m;j++) {
+	for (int i = 1;i <= n;i++) {
+		for (int j=1;j <= m;j++) {
 			cin >> L[i][j];
-			S[i][j]=
+			S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + L[i][j];
 		}
 	}
+	int x1, x2, y1, y2;
+
+
+	for (int i = 1;i <= q;i++) {
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout<<S[x2][y2]+S[x1-1][y1-1]-S[x1-1][y2] - S[x2][y1-1] << endl;
+
+	}
 	
-
-
 }
