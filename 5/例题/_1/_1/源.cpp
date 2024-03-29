f4222c5930ce21042f1b83b09cce33e5a30dada5
o.O? <932898583@qq.com>
2024-03-29 19:37:37 +0800

    1

--- 5/例题/_1/_1/源.cpp
+++ 5/例题/_1/_1/源.cpp
@@ -3,9 +3,28 @@
 # include <stdio.h>
 
 using namespace std;
+const int N = 10e5 + 10;
+int n, m;
+int q[N];
+
 
 int main (void){
+	/*cin>>n>>m;
+	for (int i = 1; i <= n; i++) {
+		cin>>q[i];
+	}
+	for (int i = 1;i <= n;i++) {
+		q[i] += q[i - 1];
+	}
 
-
+	for (int i = 1;i <= m;i++) {
+		int k, a, b;
+		cin>>k>>a>>b;
+		if(k==0)cout<<q[b]-q[a-1]<<endl;
+		if (k == 1) {
+			for(int j=a;j<=n;j++)q[j]+=b;
+		}
+	}*/
+	//前缀和会发生超时的问题 尝试线段树
 	return 0;
 }
