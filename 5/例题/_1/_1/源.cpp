5fc2b1f4a31e4932b4af0ca31cb0b3190d170445
o.O? <932898583@qq.com>
2024-11-03 13:39:53 +0800

    1

--- 5/例题/_1/_1/源.cpp
+++ 5/例题/_1/_1/源.cpp
@@ -5,8 +5,27 @@
 using namespace std;
 const int N = 10e5 + 10;
 int n, m;
-int q[N];
+int a[N];
+int c[N];
 
+
+int lowbbit(int x) {
+	return x & -x;
+}
+
+void add(int x, int v) {
+	for (int i = x; i <= n; i += lowbbit(i)) {
+		c[i] += v;
+	}
+}
+
+int check(int x) {
+	int res = 0;
+	for (int i = x; i; i -= lowbbit(i)) {
+		res += c[i];
+	}
+	return res;
+}
 
 int main (void){
 	/*cin>>n>>m;
@@ -26,5 +45,19 @@ int main (void){
 		}
 	}*/
 	//前缀和会发生超时的问题 尝试线段树
+
+
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)cin>>a[i];
+	for (int i = 1;i <= n;i++)add(i, a[i]);
+	
+		while (m--) {
+			int k, a, b;
+			cin>>k>>a>>b;
+			if (k == 0)cout << check(b) - check(a - 1) << endl;
+			else add(a, b);
+		 }
+
+
 	return 0;
 }
